d6983aeb851e60b0901f34a5d8978938b28ef615
Diogo Costa <37339406+diogocsbr@users.noreply.github.com>
2020-09-24 21:25:03 -0300

    colocando adapters no listview

---
+++ DesignerWalkthrough/DesignerWalkthrough/MainActivity.cs
@@ -0,0 +1,103 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Collections.Generic;
+using Android.Views;
+
+namespace DesignerWalkthrough
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        List<ColorItem> colorItems = new List<ColorItem>();
+        ListView listView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            listView = FindViewById<ListView>(Resource.Id.myListView);
+
+
+            colorItems.Add(new ColorItem()
+            {
+                Color = Android.Graphics.Color.DarkRed,
+                ColorName = "Dark Red",
+                Code = "8B0000"
+            });
+            colorItems.Add(new ColorItem()
+            {
+                Color = Android.Graphics.Color.SlateBlue,
+                ColorName = "Slate Blue",
+                Code = "6A5ACD"
+            });
+            colorItems.Add(new ColorItem()
+            {
+                Color = Android.Graphics.Color.ForestGreen,
+                ColorName = "Forest Green",
+                Code = "228B22"
+            });
+
+            listView.Adapter = new ColorAdapter(this, colorItems);
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+
+    }
+
+    public class ColorAdapter : BaseAdapter<ColorItem>
+    {
+        List<ColorItem> items;
+        Activity context;
+        public ColorAdapter(Activity context, List<ColorItem> items)
+            : base()
+        {
+            this.context = context;
+            this.items = items;
+        }
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+        public override ColorItem this[int position]
+        {
+            get { return items[position]; }
+        }
+        public override int Count
+        {
+            get { return items.Count; }
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var item = items[position];
+
+            View view = convertView;
+            if (view == null) // no view to re-use, create new
+                view = context.LayoutInflater.Inflate(Resource.Layout.list_item, null);
+            view.FindViewById<TextView>(Resource.Id.textView1).Text = item.ColorName;
+            view.FindViewById<TextView>(Resource.Id.textView2).Text = item.Code;
+            view.FindViewById<ImageView>(Resource.Id.imageView1).SetBackgroundColor(item.Color);
+
+            return view;
+        }
+    }
+
+    public class ColorItem
+    {
+        public string ColorName { get; set; }
+        public string Code { get; set; }
+        public Android.Graphics.Color Color { get; set; }
+    }
+}
